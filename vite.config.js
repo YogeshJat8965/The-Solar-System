70854c01a264cf868b9058d373b5e416232c7533
yogesh <yogeshjat8965@gmail.com>
2025-07-03 10:12:40 +0530

    ready to deploy

--- vite.config.js
+++ vite.config.js
@@ -12,5 +12,6 @@ export default {
         outDir: '../dist', // Output in the dist/ folder
         emptyOutDir: true, // Empty the folder first
         sourcemap: true // Add sourcemap
+        
     },
 }
